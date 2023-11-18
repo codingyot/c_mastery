8e924bb5914c841b936a5a0b705a850e371abe1c
codingyot <149825790+codingyot@users.noreply.github.com>
2023-11-18 00:16:29 +0000

    files updated

--- exercises/aff_a.c
+++ exercises/aff_a.c
@@ -1,26 +1,29 @@
 #include <unistd.h>
 
-void aff_a(char *str)
+void aff(char *str)
 {
     int i;
 
     i = 0;
-    while(str[i] != '\0')
-    {
-        if (str[i] == 'a')
-            write(1, "a", 1);
-         i++;
-    }
-    write(1, "\n", 1);
-   
+		while (str[i] != '\0')
+		{
+			if (str[i] == 'a')
+			{
+				write(1, "a", 1);
+				break ;
+			}
+			i++;
+		}
+		write(1, "\n", 1);
 }
 
-int main(int ac, char **av)
+int		main(int ac, char **av)
 {
     if (ac == 2)
     {
-        aff_a(av[1]);
-    }else
+        aff(av[1]);
+    }
+    else
         write(1, "a\n", 2);
     return 0;
 }
--- exercises/ft_range.c
+++ exercises/ft_range.c
@@ -1,58 +1,32 @@
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oallan <oallan@student.42abudhabi.ae>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 14:35:54 by oallan            #+#    #+#             */
+/*   Updated: 2023/11/10 14:36:04 by oallan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int PandN(int y)
+int	*ft_range(int min, int max)
 {
-    if (y < 0)
-        return (-y);
-    return (y);
-}
-int     *ft_range(int start, int end)
-{
-    int i;
-    int a;
-    int *w;
-    int *x;
+	int		*array;
+	unsigned int	i;
 
-    a = PandN(end - start) + 1;
-    w = (int *)malloc(sizeof(int) * a);
-    if (!w)
-        return (0);
-    if (a == 1)
-        x[0] = start;
-    i = 0;
-    x = w;
-    if (start < end)
-    {
-        while(a > i)
-        {
-            x[i] = start + i;
-            i++;
-        }
-    }
-    else if (start > end)
-    {
-        while(a > i)
-        {
-            x[i] = start - i;
-            i++;
-        }
-    }
-    return (x);
-}
-int main()
-{
-    int i = 0;
-    int start = -100;
-    int end = 2;
-    int *x;
-
-    int a = PandN(end - start) + 1;
-    x = ft_range(start, end);
-    while (a > i)
-    {
-        printf("%i, ", x[i]);
-        i++;
-    }
-    free(x);
+	i = 0;
+	if (min >= max)
+		return (NULL);
+	array = (int *)malloc(sizeof(int) * (max - min));
+	while (min < max)
+	{
+		array[i] = min;
+		min++;
+		i++;
+	}
+	return (array);
 }
