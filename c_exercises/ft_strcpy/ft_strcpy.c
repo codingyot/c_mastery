8b1ffa982998ca6fc7b0b4c359ee09e0c00c9d21
codingyot <149825790+codingyot@users.noreply.github.com>
2023-11-20 12:26:19 +0000

    ft_strcpy added

---
+++ c_exercises/ft_strcpy/ft_strcpy.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <stdio.h>
+
+char *ft_strcpy(char *dest, char *src)
+{
+    int i;
+
+    i = 0;
+    while(src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
+
+int main()
+{
+    char x[] = "hello";
+    char y[] = "hi";
+    printf("%s", ft_strcpy(x, y));
+}
